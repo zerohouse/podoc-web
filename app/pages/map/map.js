0e9ca74f5977fa1cf7d94c32d1ad0d69257bced0
dev63 <parksungho86@gmail.com>
2016-09-02 01:03:23 +0900

    map updated

--- app/pages/map/map.js
+++ app/pages/map/map.js
@@ -1,27 +1,80 @@
 (function () {
     angular.module('podoc.controllers').controller('pokemonMapCtrl', pokemonMapCtrl);
     /* @ng-inject */
-    function pokemonMapCtrl($scope, $ajax, Pokemons, $interval, $stateParams) {
+    function pokemonMapCtrl($scope, $ajax, Pokemons, $interval, $stateParams, $timeout) {
 
+        $scope.$on('$stateChangeSuccess', function () {
+            if ($scope.map && $scope.map.control && $scope.map.control.refresh)
+                $scope.map.control.refresh();
+            if ($stateParams.lat && $stateParams.lng) {
+                $scope.map.center.latitude = $stateParams.lat;
+                $scope.map.center.longitude = $stateParams.lng;
+            }
+        });
 
-        var lat = 38.2039839397;
-        var lng = 128.590853994;
-        $scope.markers = [];
-        var d = 0.0004;
+        $scope.refresh = function () {
+            if (!$scope.bounds)
+                return;
+            $ajax.get('/api/pokemons', getBoundsObj()).then(function (pokemons) {
+                pokemons.forEach(function (pokemon) {
+                    if ($scope.pokemons.findById(pokemon.id))
+                        return;
+                    pokemon.pokemon = Pokemons.get(pokemon.pokemon_id);
+                    pokemon.expirationTime = new Date(pokemon.expirationTime);
+                    pokemon.icon = {
+                        url: "/" + pokemon.pokemon.img,
+                        scaledSize: new google.maps.Size(50, 50),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(25, 0)
+                    };
+                    $scope.pokemons.push(pokemon);
+                });
+            });
+        };
 
-
-        for (var i = 0; i < 5; i++) {
-            var latd = d * i;
-            for (var j = 0; j < 5; j++) {
-                var lngd = d * j;
-                $scope.markers.push({x: lat + latd, y: lng + lngd});
+        function getBoundsObj() {
+            if (!$scope.bounds)
+                return;
+            return {
+                minLat: $scope.bounds.f.f,
+                maxLat: $scope.bounds.f.b,
+                minLng: $scope.bounds.b.b,
+                maxLng: $scope.bounds.b.f
             }
         }
 
-        console.log($scope.markers);
+        $interval(function () {
+            $scope.pokemons.forEach(pokemon=> {
+                var remainMs = pokemon.expirationTime.getTime() - new Date().getTime();
+                if (remainMs > 0)
+                    pokemon.remainTime = moment(remainMs).format('mm:ss');
+                else {
+                    $scope.pokemons.remove(pokemon);
+                }
+            })
+        }, 1000);
+
+        $interval($scope.refresh, 2000);
 
 
+        $scope.scanner = {};
         $scope.pokemons = [];
+
+        function requestScan(lat, lng) {
+            // var map = $scope.map.control.getGMap();
+            // var lat = map.center.lat();
+            // var lng = map.center.lng();
+            $ajax.post('/api/point', {lat: lat, lng: lng});
+        }
+
+        var time;
+        $scope.requestBoundaries = function () {
+            $timeout.cancel(time);
+            time = $timeout(function () {
+                $ajax.post('/api/bounds', getBoundsObj());
+            }, 300);
+        };
+
 
         if (!$scope.map)
             $scope.map = {
@@ -34,64 +87,16 @@
                 events: {
                     bounds_changed: function (e) {
                         $scope.bounds = e.getBounds();
+                        // $scope.requestBoundaries();
+                    },
+                    click: function (mapModel, eventName, originalEventArgs) {
+                        var latitude = originalEventArgs[0].latLng.lat();
+                        var longitude = originalEventArgs[0].latLng.lng();
+                        $scope.scanner.center = {latitude: latitude, longitude: longitude};
+                        requestScan(latitude, longitude);
                     }
                 }
             };
 
-
-        $scope.$on('$stateChangeSuccess', function () {
-            if ($scope.map && $scope.map.control && $scope.map.control.refresh)
-                $scope.map.control.refresh();
-            if ($stateParams.lat && $stateParams.lng) {
-                $scope.map.center.latitude = $stateParams.lat;
-                $scope.map.center.longitude = $stateParams.lng;
-            }
-        });
-
-        $scope.getLabel = function (pokemon) {
-            return getTwo(Math.floor(pokemon.pokemon_remain_time / 60)) + ":" + getTwo(pokemon.pokemon_remain_time % 60);
-        };
-
-        function getTwo(num) {
-            if (num > 9)
-                return num;
-            return "0" + num;
-        }
-
-        $scope.refresh = function () {
-            if (!$scope.bounds)
-                return;
-            $ajax.get('/map/api/' + $scope.bounds.f.f + "/" + $scope.bounds.f.b + "/" + $scope.bounds.b.b + "/" + $scope.bounds.b.f).then(null, function (res) {
-                var pokemons = res.list;
-
-                pokemons.forEach(function (pokemon) {
-                    var exist = $scope.pokemons.findBy("pokemon_id", pokemon.pokemon_id);
-                    if (exist)
-                        exist.pokemon_remain_time = pokemon.pokemon_remain_time;
-                    else {
-                        pokemon.pokemon = Pokemons.get(parseInt(pokemon.pokemon_type));
-                        pokemon.icon = {
-                            url: "/" + pokemon.pokemon.img,
-                            scaledSize: new google.maps.Size(50, 50),
-                            origin: new google.maps.Point(0, 0),
-                            anchor: new google.maps.Point(0, 25)
-                        };
-                        $scope.pokemons.push(pokemon);
-                    }
-                });
-
-                $scope.pokemons.forEach(pokemon=> {
-                    var finded = pokemons.findBy("pokemon_id", pokemon.pokemon_id);
-                    if (!finded)
-                        $scope.pokemons.pokemon_remain_time--;
-                    if ($scope.pokemons.pokemon_remain_time <= 2)
-                        $scope.pokemons.remove(pokemon);
-                });
-
-
-            });
-        };
-
-        $interval($scope.refresh, 1000);
     }
 })();
